3346ebad53b55be5894aabaa07a7cf5941142153
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-02-02 08:41:32 +0200

    Consumer now uses asyn job

--- src/consumer.js
+++ src/consumer.js
@@ -4,13 +4,12 @@ import Bull from "bull"
 const consumer = function() {
   const imageProcessingQueue = new Bull('processing-image');
 
-  imageProcessingQueue.process(function(job, done) {
+  imageProcessingQueue.process(async function(job) {
     console.log("job.id", job.id, "job.data", job.data);
 
     longRunningFunction(job.data.imageURL);
 
-    done(null, {imageURL: job.data.imageURL, result: "OK"});
-
+    return {imageURL: job.data.imageURL, result: "OK"};
   });
 };
 
